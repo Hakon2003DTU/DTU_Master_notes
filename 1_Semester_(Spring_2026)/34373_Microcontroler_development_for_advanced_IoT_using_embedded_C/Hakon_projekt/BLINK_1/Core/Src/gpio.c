c6547cab229fa8ca6632363f7f17ed6889ba312e
Hakon2003DTU <hakonhlyn@gmail.com>
2026-02-07 13:07:18 +0100

    blink 1

--- 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Hakon_projekt/BLINK_1/Core/Src/gpio.c
+++ 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Hakon_projekt/BLINK_1/Core/Src/gpio.c
@@ -51,14 +51,14 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
--- 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Hakon_projekt/BLINK_1/Core/Src/main.c
+++ 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Hakon_projekt/BLINK_1/Core/Src/main.c
@@ -61,45 +61,34 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
+
+
+/*
+ *
+ * */
+
+
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  uint8_t X = 0;
   while (1)
   {
+	  for (int i = 0; i < X; i++){
 
-	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-	  HAL_Delay(100);
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);	//Reset LED
+		  HAL_Delay(100);												//Delay 100ms
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);		//Set LED
+	  	  HAL_Delay(100);												//Delay 100ms
 
+	  }
+	  HAL_Delay(1000);
+	  X++;
   }
-  /* USER CODE END 3 */
 }
 
 /**
