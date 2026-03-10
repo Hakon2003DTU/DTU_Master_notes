8f743aa02a2091f9efee20565fb6093bf822d6e5
Hakon2003DTU <hakonhlyn@gmail.com>
2026-03-10 15:14:52 +0100

    update

--- 1_Semester_(Spring_2026)/34349_FPGA_design_for_communication_systems/FIFO_Read_Control.vhd
+++ 1_Semester_(Spring_2026)/34349_FPGA_design_for_communication_systems/FIFO_Read_Control.vhd
@@ -7,7 +7,7 @@ entity FIFO_Read_Control is
 		reset : in std_logic;
 		rclk : in std_logic;
 		read_enable : in std_logic;
-		read_data_in : in std_logic_vector(7 downto 0);
+		W_sync : in std_logic_vector(3 downto 0);
 		empty : out std_logic;
 		ren : out std_logic;
 		fifo_occu_out : out std_logic_vector(4 downto 0);
@@ -17,9 +17,36 @@ entity FIFO_Read_Control is
 end FIFO_Read_Control;--finished
 
 architecture behavioral of FIFO_Read_Control is
-    -- Signals would be declared here [cite: 6]
+
+    -- Signals would be declared here
+
+	signal Readpointer: std_logic_vector(4 downto 0); 
+
+
+
 begin
 
+
+
+
+process(rclk,reset)
+	begin
+		if (reset = '1') then
+		
+		elsif rising_edge(rclk) then
+
+
+		
+			-- Defining when the the system is empty  
+			if (wptr == W_sync) then 
+				empty <= '1'; 
+			else 
+				empty <='0';
+			end if;
+		end if;
+		fifo_occu_out <= W_sync - Readpointer;
+		rptr <= Readpointer;
+	end process
 
 
 end behavioral;
